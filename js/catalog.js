558d1eca2f53d1358ba8ccee3d04b3d36a958ce9
petruhin-pavel <89390635+petruhin-pavel@users.noreply.github.com>
2021-11-03 15:40:41 +0300

    работа с js карточка

--- js/catalog.js
+++ js/catalog.js
@@ -2,22 +2,55 @@ let bottleCol = document.getElementById('bottle_col');
 let bottleMinus = document.getElementById('bottle_minus');
 let bottlePlus = document.getElementById('bottle_plus');
 
+const bottelsVolume = document.getElementsByClassName('card__select__volume');
+
+bottelsVolume.onclick = function () {
+  console.log('Клик');
+}
+
+
+
+
+/* const bottle375 = document.getElementById('bottle375');
+const bottle750 = document.getElementById('bottle750');
+ */
+
+/* bottle750.onclick = function () {
+  if (bottle750.checked) {
+    console.log('123');
+  }
+}
+ */
+
+
+
+/* const chooseVolume = function () {
+ 
+} */
+
+
+/* const bottle375 = 400;
+const bottle750 = 600; */
+
 bottlePlus.onclick = function () {
   bottleCol.value = parseInt(bottleCol.value) + 1;
+  checkColBottle();
 }
 
 bottleMinus.onclick = function () {
   bottleCol.value = parseInt(bottleCol.value) - 1;
+  checkColBottle();
 }
 
 
-
-/*   if (bottleCol.value <= 1) {
+function checkColBottle() {
+  if (bottleCol.value <= 1) {
     bottleMinus.setAttribute('disabled', true);
   } else {
     bottleMinus.removeAttribute('disabled');
   }
- */
+};
+
 
 
 
