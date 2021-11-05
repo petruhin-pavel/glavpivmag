09e5d4c14197959d1e69cf7c4cb40e03c62189c8
petruhin-pavel <89390635+petruhin-pavel@users.noreply.github.com>
2021-11-05 17:13:10 +0300

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -2,35 +2,12 @@ let bottleCol = document.getElementById('bottle_col');
 let bottleMinus = document.getElementById('bottle_minus');
 let bottlePlus = document.getElementById('bottle_plus');
 
-const bottelsVolume = document.getElementsByClassName('card__select__volume');
+const bottelsVolume = document.getElementsByName('select__volume');
 
-bottelsVolume.onclick = function () {
+bottelsVolume.onfocus = function () {
   console.log('Клик');
 }
 
-
-
-
-/* const bottle375 = document.getElementById('bottle375');
-const bottle750 = document.getElementById('bottle750');
- */
-
-/* bottle750.onclick = function () {
-  if (bottle750.checked) {
-    console.log('123');
-  }
-}
- */
-
-
-
-/* const chooseVolume = function () {
- 
-} */
-
-
-/* const bottle375 = 400;
-const bottle750 = 600; */
 
 bottlePlus.onclick = function () {
   bottleCol.value = parseInt(bottleCol.value) + 1;
@@ -54,3 +31,23 @@ function checkColBottle() {
 
 
 
+/*
+const bottle375 = document.getElementById('bottle375');
+const bottle750 = document.getElementById('bottle750');
+
+
+  bottle750.onclick = function () {
+  if (bottle750.checked) {
+    console.log('123');
+  }
+}
+
+const chooseVolume = function () {
+
+const bottle375 = 400;
+const bottle750 = 600;
+
+bottelsVolume.onclick = function () {
+  console.log('Клик');
+}
+*/
